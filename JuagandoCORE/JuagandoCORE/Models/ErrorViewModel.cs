032210239e1d57d9cf0cd8c98ed77903ae5d0e36
juan manuel moyano ceballos <jmnmoyano@hotmail.com>
2020-09-24 19:51:20 +0200

    comit 2

---
+++ JuagandoCORE/JuagandoCORE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JuagandoCORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
